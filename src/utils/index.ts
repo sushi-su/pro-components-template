eb522bc6395a34315f5f176ac04d9fbe561fe734
x_x <s.xiuxing@foxmail.com>
2022-03-03 17:48:37 +0800

    feat: request

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './lazyload';
+export * from './request';
---
+++ src/utils/request.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
+export const request = axios;
