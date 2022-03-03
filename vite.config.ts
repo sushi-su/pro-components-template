5653e56a8fca710ae82b0f78f900f2dcbc72467c
x_x <s.xiuxing@foxmail.com>
2022-03-03 22:03:34 +0800

    chore: 增加打包依赖分析

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const plugins: UserConfig['plugins'] = [react()];
+
+if (process.env.vis) {
+  plugins.push(visualizer({ open: true, gzipSize: true, brotliSize: true }));
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins,
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
