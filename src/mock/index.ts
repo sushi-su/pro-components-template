70a5fc148236b2bc3f05cca160e4ab5ae9da11cf
Sushi Su <s.xiuxing@foxmail.com>
2022-04-18 16:38:49 +0800

    feat: handleRequestError

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,7 +1,6 @@
 import Mockjs from 'mockjs';
 import './account';
 
-console.log(11);
 Mockjs.setup({
   timeout: 500 - 1000,
 });
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,49 @@
 import type { API } from '@/typings';
+import { message, notification } from 'antd';
 import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+enum ErrorShowType {
+  SILENT = 0,
+  WARN_MESSAGE = 1,
+  ERROR_MESSAGE = 2,
+  NOTIFICATION = 4,
+  REDIRECT = 9,
+}
+
+type Request = <D>(config: AxiosRequestConfig) => Promise<API.Response<D>>;
+
+const handleRequestError = async (errorShowType: ErrorShowType, msg: string) => {
+  const navigate = useNavigate();
+  switch (errorShowType) {
+    case ErrorShowType.SILENT:
+      // do nothing
+      break;
+
+    case ErrorShowType.WARN_MESSAGE:
+      message.warn(msg);
+      break;
+
+    case ErrorShowType.NOTIFICATION:
+      notification.warn({
+        message: msg,
+      });
+      break;
+
+    case ErrorShowType.ERROR_MESSAGE:
+      message.error(msg);
+      break;
+
+    case ErrorShowType.REDIRECT:
+      message.error(msg);
+      navigate('/login');
+      break;
+
+    default:
+      break;
+  }
+};
 
 const instance = axios.create({
   baseURL: 'https://some-domain.com/api/',
@@ -33,8 +76,6 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   },
 );
-
-type Request = <D>(config: AxiosRequestConfig) => Promise<API.Response<D>>;
 
 export const request: Request = async (config) => {
   const { data } = await instance(config);
