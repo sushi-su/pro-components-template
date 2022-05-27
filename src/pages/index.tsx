6b706595c8557a83a5c8c3578d3f31d7b204b4e2
Sushi Su <s.xiuxing@foxmail.com>
2022-05-27 19:13:42 +0800

    chore: umi 4

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+  return <PageContainer>Index</PageContainer>;
+};
+
+export default HomePage;
