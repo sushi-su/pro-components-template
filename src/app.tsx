f2cdac33700a09d1c7475a1ea9f69308143c9bb2
Sushi Su <s.xiuxing@foxmail.com>
2022-06-09 19:01:38 +0800

    chore(deps): update

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,22 @@
-export const getInitialState = () => {
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import type { ReactNode } from 'react';
+import { createElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+dayjs.locale('zh-cn');
+
+const queryClient = new QueryClient();
+
+export { request } from '@/runtime';
+
+export const getInitialState = async () => {
   return {
     userName: '用户名',
     userId: '用户 ID',
   };
 };
 
-export const layout = {
-  logout: () => {}, // do something
-};
-
-export { request } from '@/runtime';
+export function dataflowProvider(container: ReactNode) {
+  return createElement(QueryClientProvider, { client: queryClient }, container);
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,14 @@
-import { PageContainer } from '@ant-design/pro-components';
+import { PageContainer, ProForm, ProFormDatePicker } from '@ant-design/pro-components';
 import type { FC } from 'react';
 
 const HomePage: FC = () => {
-  return <PageContainer>Index</PageContainer>;
+  return (
+    <PageContainer>
+      <ProForm>
+        <ProFormDatePicker />
+      </ProForm>
+    </PageContainer>
+  );
 };
 
 export default HomePage;
